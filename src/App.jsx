230a7cd780e3cad75f961cc7f06efbd8bafafaec
paulius <paulius@2simple.com>
2023-02-26 21:05:03 +0000

    add core style utilities and app backbone

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
-
-function App(props) {
+import styles from "./styles.js";
+const App = (props) => {
     return (
-        <div>Clean</div>
+        <div className="bg-primary w-full overflow-hidden">
+            <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+                <div className={`${styles.boxWidth}`}>
+                    NavBar
+                </div>
+            </div>
+
+            <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+                <div className={`${styles.boxWidth}`}>
+                    HERO COMPONENT
+                </div>
+            </div>
+
+            <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+                <div className={`${styles.boxWidth}`}>
+                    Stats
+                    Business
+                    billing
+                    cardeal
+                    testimodials
+                    clients
+                    CTA
+                    Footer
+                </div>
+            </div>
+        </div>
     );
 }
 
