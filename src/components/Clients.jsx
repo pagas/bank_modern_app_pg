2953fb6ee56e069cc54e0da3105a83749dd01c08
paulius <paulius@2simple.com>
2023-03-11 16:48:33 +0000

    add hover color on client components

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -4,9 +4,10 @@ import styles from "../styles.js";
 
 const Clients = () => (
     <section id="clients" className={`${styles.flexCenter} my-4`}>
-        <div className={`${styles.flexCenter} flex-wrap w-full`}>
+        <div className={`${styles.flexCenter} flex-wrap w-full items-stretch`}>
             {clients.map(client => (
-                <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[190px] min-w-[120px]`}>
+                <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[190px] min-w-[120px]
+                hover:rounded-full hover:bg-dimBlue`}>
                     <img src={client.logo} alt="client" className="sm:w-[192px] w-[100px] object-contain"/>
                 </div>
             ))}
